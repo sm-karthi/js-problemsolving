61b0aa71a281cfb8619d19fe62ba097162c99d8b
sm-karthi <karthikeyan24022007@gmail.com>
2024-09-27 19:06:21 +0530

    commit in js fille

--- javascript/slackSum.js
+++ javascript/slackSum.js
@@ -1,4 +1,4 @@
-// 1st sum
+/* // 1st sum
 let char = "A";
 if(char === "a" || char === "e" || char === "i" || char === "o" || char === "u"){
     console.log("The character is a Vowel.");   
@@ -47,4 +47,88 @@ function printAllMultiplesOf5Bet(a, b){
         }
     }
 }
-printAllMultiplesOf5Bet(10,30);
+printAllMultiplesOf5Bet(10,30); */
+
+//27/9/2024 Slack Sums
+// 1 st sum
+/* function multipleOf5(IntegerArr){
+    let count = 0;
+    for (let i = 0; i < IntegerArr.length; i++){
+        if( IntegerArr[i] % 5 === 0){
+            count++;
+        }
+    }
+    return count;
+
+}
+console.log(multipleOf5([2, 15, 67, 90, 31]));
+
+// 2nd sum
+function AbetweenB(arr , a , b){
+    let count = 0;
+    let str = "";
+    for (let i =0; i < arr.length; i++){
+        if(arr[i] > a && arr[i] < b){
+            count++;
+            str += arr[i]+" ";
+        }
+    }
+    console.log(str+ "Total Number : "+count);
+   
+}
+AbetweenB([3, 5, 20, 7, 8] , 4, 9);
+
+
+// 3rd sum
+ function vowels(arr){
+    count=0;
+    for(let i=0; i<arr.length; i++){
+        if(arr[i][0]==='a' || arr[i][0]==='A' || arr[i][0]==='e' || arr[i][0]==='E'|| arr[i][0]==='i' || arr[i][0]==='I' || arr[i][0]==='o' || arr[i][0]==='O' || arr[i][0]==='u' || arr[i][0]==='U'){
+            count++;
+        }
+    }
+        return count
+}
+let arr=["Orange","Apple","banana"]
+console.log(vowels(arr))
+
+// 4th sum
+function Average(floatArr){
+    let sum = 0;
+    for(let i  = 0; i < floatArr.length; i++){
+        sum += floatArr[i];
+    }
+    return Math.floor(sum/floatArr.length);
+}
+console.log(Average([2.5, 7.7, 20.2, 5.3]));
+
+// practice sum
+let Device = [
+    { Name : "Laptop", price : 800},
+    { Name : "Phone", price : 500},
+    { Name : "Computer", price : 2000}
+];
+
+for(let i = Device.length-1; i >= 0; i--){
+    console.log(Device[i].price);
+    
+}
+
+
+ 
+  */
+
+let a = 121;
+/* let b = [...a.toString()].map(Number);
+//console.log(b);
+let c = [...b].reverse();
+//console.log(c);
+if(b.join() === c.join()){s
+    console.log("Yes");
+}
+else{
+    console.log("No");
+}
+
+ */
+console.log(Number(a.toString().split("").reverse().join(""))==a?"Yes":"NO");
